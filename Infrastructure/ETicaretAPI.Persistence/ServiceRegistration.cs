76ce9cca48b8044a7ff9f94a388acb538d92c832
Burak Özyolu <33603280+BurakOzyolu@users.noreply.github.com>
2023-04-13 12:17:52 +0300

    test data checked

---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        //IOC Container'da her bir IProductService isteğine karşılık olarak bir tane ProductService nesnesi üretecek. Buradaki extention metodu yazılmalıdır. Bu extention metodu IoC Container'a gönderilecek.
+        //Extention Metod
+        public static void AddPersistenceServices( this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
